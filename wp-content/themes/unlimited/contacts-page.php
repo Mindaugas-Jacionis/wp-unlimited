67010b5dcecda3df802a898e1e4e2145f127606e
m.jacionis <m.jacionis>
2016-04-21 23:34:09 +0300

    contact page initial

--- wp-content/themes/unlimited/contacts-page.php
+++ wp-content/themes/unlimited/contacts-page.php
@@ -13,7 +13,17 @@ get_header(); ?>
 	<div class="content-wrap">
 		<h1 class="page-headline"><?php the_title(); ?></h1>
 		<div class="contacts-list">
-			
+			<?php
+	            $args = array( 'post_type' => 'location', 'posts_per_page' => 1000 );
+	            $loop = new WP_Query( $args );
+	            if ( $loop->have_posts() ) {
+	                while ( $loop->have_posts() ) {
+	                    $loop->the_post();
+	                    get_template_part( 'content-contact', get_post_format() );
+	                }
+	            }
+	            wp_reset_postdata(); // reset to the original page data
+	        ?>
 		</div>
 		<div class="content-bottom">
 			<?php if( !empty($contactForm) ): ?>
